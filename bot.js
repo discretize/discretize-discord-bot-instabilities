a4cb5c9a382adec7bef31d60e2412010fa751a75
Princeps <21026467+gw2princeps@users.noreply.github.com>
2020-10-13 00:25:53 +0200

    feat: added discord dT rank API

--- bot.js
+++ bot.js
@@ -1,9 +1,10 @@
 const db = require("./database.js");
 const processLog = require("./processLog.js");
-const instab = require('./instability.js');
+const instab = require("./instability.js");
 
-const getUrls = require('get-urls');
-const request = require('request');
+const http = require("http");
+const getUrls = require("get-urls");
+const request = require("request");
 
 const Discord = require("discord.js");
 
@@ -12,6 +13,8 @@ const csv = require("csv-parser");
 const fs = require("fs");
 const results = [];
 var CronJob = require("cron").CronJob;
+
+const port = process.env.PORT || 3000;
 
 client.on("ready", () => {
     client.user.setActivity("Healbrands", {type: "WATCHING"});
@@ -37,14 +40,16 @@ client.on("ready", () => {
 });
 
 function sendHelp(channel) {
-    channel.send("```**HELP MENU** - Discretize [dT] bot \n \
-- !today - shows todays instabilities\n \
-- !tomorrow - shows tomorrows instabilities\n \
-- !upload <optional: categories> <dps.report link> - uploads a log\n \
-- !percentiles <dps.report link> - shows percentiles for a log\n \
-- !verify <api-key> - api-key must be named dtlogbot \n \
-- !addcategory <dps.report link> - adds categories to logs to filter\
-        ```");
+    channel.send(
+        "```**HELP MENU** - Discretize [dT] bot \n \
+    - !today - shows todays instabilities\n \
+    - !tomorrow - shows tomorrows instabilities\n \
+    - !upload <optional: categories> <dps.report link> - uploads a log\n \
+    - !percentiles <dps.report link> - shows percentiles for a log\n \
+    - !verify <api-key> - api-key must be named dtlogbot \n \
+    - !addcategory <dps.report link> - adds categories to logs to filter\
+            ```"
+    );
 }
 
 client.on("message", (message) => {
@@ -66,30 +71,30 @@ function uploadLogs(urls, message) {
         if (value.startsWith("https://dps.report/")) {
             let permalink = value.substr(19);
 
-            request('https://dps.report/getJson?permalink=' + permalink, {json: true}, (err, res, body) => {
-                if (err) {
-                    return console.log(err);
+            request(
+                "https://dps.report/getJson?permalink=" + permalink,
+                {json: true},
+                (err, res, body) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    db.insertLog(body, permalink);
+                    categories.forEach((cat) => {
+                        db.addCategory(permalink, cat);
+                    });
+                    message.react("👍");
                 }
-                db.insertLog(body, permalink);
-                categories.forEach((cat) => {
-                    db.addCategory(permalink, cat);
-                });
-                message.react("👍");
-            });
+            );
         } else {
             message.channel.send("Message is not a valid dps.report link: " + value);
         }
     });
 }
 
-client.login(process.env.BOT_TOKEN);
-db.connect();
-
 client.on("message", (message) => {
     let args = message.content.split(" ");
 
     if (args[0].toLowerCase() === "!upload") {
-
         let urls = [];
         if (message.attachments.size > 0) {
             message.attachments.mapValues((v, k) => {
@@ -103,7 +108,6 @@ client.on("message", (message) => {
         } else {
             uploadLogs(getUrls(message.content), message);
         }
-
     } else if (args[0].toLowerCase() === "!stats") {
         if (args.length <= 1) {
             // display help
@@ -115,8 +119,14 @@ client.on("message", (message) => {
                 const categories = getCategories(message.content, 2);
 
                 db.allPercentiles(categories, (results) => {
-                    message.channel.send(processLog.getPersonPercentileEmbed(Discord, categories.toString(), results));
-                })
+                    message.channel.send(
+                        processLog.getPersonPercentileEmbed(
+                            Discord,
+                            categories.toString(),
+                            results
+                        )
+                    );
+                });
                 break;
             case "self":
                 // give info about personal percentages for verified users
@@ -124,11 +134,19 @@ client.on("message", (message) => {
                     if (result.length === 1) {
                         if (result[0].verified) {
                             db.personPercentile(result[0].acc, function (res2) {
-                                message.channel.send(processLog.getPersonPercentileEmbed(Discord, result[0].acc, res2));
+                                message.channel.send(
+                                    processLog.getPersonPercentileEmbed(
+                                        Discord,
+                                        result[0].acc,
+                                        res2
+                                    )
+                                );
                             });
                         }
                     } else {
-                        message.channel.send("Your account is not verified yet! Use !verify <api key>!")
+                        message.channel.send(
+                            "Your account is not verified yet! Use !verify <api key>!"
+                        );
                     }
                 });
                 break;
@@ -138,16 +156,22 @@ client.on("message", (message) => {
                     if (value.startsWith("https://dps.report/")) {
                         let permalink = value.substr(19);
                         db.partyPercentile(permalink, function (party, members) {
-                            let embed = processLog.getPercentileEmbed(Discord, party, members, permalink);
+                            let embed = processLog.getPercentileEmbed(
+                                Discord,
+                                party,
+                                members,
+                                permalink
+                            );
                             message.channel.send(embed);
                         });
                     } else {
-                        message.channel.send("Message is not a valid dps.report link: " + value);
+                        message.channel.send(
+                            "Message is not a valid dps.report link: " + value
+                        );
                     }
                 });
                 break;
         }
-
     } else if (args[0].toLowerCase() === "!addcategory") {
         const urls = getUrls(message.content);
         const categories = getCategories(message.content, 1);
@@ -156,10 +180,12 @@ client.on("message", (message) => {
                 let permalink = value.substr(19);
                 categories.forEach((cat) => {
                     db.addCategory(permalink, cat);
-                })
+                });
                 message.react("👍");
             } else {
-                message.channel.send("Message is not a valid dps.report link: " + value);
+                message.channel.send(
+                    "Message is not a valid dps.report link: " + value
+                );
             }
         });
     } else if (args[0].toLowerCase() === "!verify") {
@@ -169,16 +195,26 @@ client.on("message", (message) => {
         }
         const api = args[1];
 
-        request("https://api.guildwars2.com/v2/tokeninfo?access_token=" + api, {json: true}, (err, res, body) => {
-            if (body.name === 'dtlogbot') {
-                request("https://api.guildwars2.com/v2/account?access_token=" + api, {json: true}, (err, res, body2) => {
-                    db.verify(body2.name, message.author.tag)
-                    message.react("👍");
-                });
-            } else {
-                message.channel.send("Invalid API-Key name! It must be named 'dtlogbot' not " + body.name);
+        request(
+            "https://api.guildwars2.com/v2/tokeninfo?access_token=" + api,
+            {json: true},
+            (err, res, body) => {
+                if (body.name === "dtlogbot") {
+                    request(
+                        "https://api.guildwars2.com/v2/account?access_token=" + api,
+                        {json: true},
+                        (err, res, body2) => {
+                            db.verify(body2.name, message.author.tag);
+                            message.react("👍");
+                        }
+                    );
+                } else {
+                    message.channel.send(
+                        "Invalid API-Key name! It must be named 'dtlogbot' not " + body.name
+                    );
+                }
             }
-        });
+        );
     }
 });
 
@@ -201,3 +237,45 @@ function getCategories(cnt, start) {
     }
     return categories;
 }
+
+client.login(process.env.BOT_TOKEN);
+db.connect();
+
+const server = http.createServer((req, res) => {
+    if (req.method !== "GET") {
+        res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+    } else {
+        if (req.url === "/getDiscordRanks") {
+            res.setHeader("Content-Type", "application/json");
+            client.guilds.fetch(process.env.DT_DISCORD_ID)
+                .then(guild => {
+                    guild.members.fetch().then(members => {
+                        let grind_count = 0;
+                        let trial_count = 0;
+                        for (let i = 0; i < members.array().length; i++) {
+                            let v = members.array()[i];
+                            v.roles.cache.forEach((e) => {
+                                if (e.name === "Grind") {
+                                    grind_count++;
+                                }
+                                if (e.name === "Trial") {
+                                    trial_count++;
+                                }
+                            })
+                        }
+                        let a = JSON.stringify({grind_count: grind_count, trial_count: trial_count});
+                        res.writeHead(200);
+                        res.end(a);
+                    })
+                })
+                .catch(console.error);
+        }else{
+            res.statusCode = 404
+            res.end(`{"error": "404"}`)
+        }
+    }
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});

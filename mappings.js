1cb32d90490fd46fcdbff03fa0799d7f5db2ac6b
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2021-10-03 12:25:56 +0200

    feat: integrate instab library, refactor code

--- mappings.js
+++ mappings.js
@@ -1,54 +1,43 @@
-exports.bosses = {
-    17632: "Skorvald",
-    17949: "Artsariiv",
-    17759: "Arkk",
-    17021: "MAMA",
-    17028: "Siax",
-    16948: "Ensolyss",
-    23254: "Disco Pogo",
-}
+exports.fractals = [
+  { name: "Aetherblade", id: 2948, level: 96 }, // 0
+  { name: "Aquatic Ruins", id: 2956, level: 76 }, // 1
+  { name: "Captain Mai Trin", id: 2932, level: 95 }, // 2
+  { name: "Chaos Isles", id: 3038, level: 97 }, // 3
+  { name: "Cliffside", id: 2930, level: 94 }, // 4
+  { name: "Deepstone", id: 4224, level: 84 }, // 5
+  { name: "Molten Boss", id: 2966, level: 90 }, // 6
+  { name: "Molten Furnace", id: 2903, level: 83 }, // 7
+  { name: "Nightmare", id: 3177, level: 98, cm: true }, // 8
+  { name: "Shattered Observatory", id: 3458, level: 99, cm: true }, // 9
+  { name: "Sirens Reef", id: 4494, level: 78 }, // 10
+  { name: "Snowblind", id: 2952, level: 93 }, // 11
+  { name: "Solid Ocean", id: 2923, level: 80 }, // 12
+  { name: "Swampland", id: 2892, level: 89 }, // 13
+  { name: "Thaumnova Reactor", id: 2967, level: 82 }, // 14
+  { name: "Twilight Oasis", id: 3973, level: 87 }, // 15
+  { name: "Uncategorized", id: 2939, level: 91 }, // 16
+  { name: "Underground Facility", id: 2947, level: 81 }, // 17
+  { name: "Urban Battleground", id: 2941, level: 85 }, // 18
+  { name: "Volcanic", id: 2989, level: 92 }, // 19
+  { name: "Sunqua Peak", id: 5446, level: 100, cm: true }, // 20
+];
 
-exports.instabDict = {
-    1: "Adrenaline Rush",
-    2: "Afflicted",
-    3: "Boon Overload",
-    4: "Flux Bomb",
-    5: "Fractal Vindicators",
-    6: "Frailty",
-    7: "Hamstrung",
-    8: "Last Laugh",
-    9: "Mists Convergence",
-    10: "No Pain, No Gain",
-    11: "Outflanked",
-    12: "Social Awkwardness",
-    13: "Stick Together",
-    14: "Sugar Rush",
-    15: "Toxic Trail",
-    16: "Vengeance",
-    17: "We Bleed Fire",
-    18: "Toxic Sickness",
-};
+exports.ROTATION_START = new Date(2021, 8, 26);
 
-exports.fractalDict = {
-    A: "Aetherblade",
-    B: "Aquatic Ruins",
-    C: "Captain Mai Trin",
-    D: "Chaos Isles",
-    E: "Cliffside",
-    F: "Deepstone",
-    G: "Molten Boss",
-    H: "Molten Furnace",
-    I: "Nightmare",
-    J: "Shattered Observatory",
-    K: "Sirens Reef",
-    L: "Snowblind",
-    M: "Solid Ocean",
-    N: "Swampland",
-    O: "Thaumnova Reactor",
-    P: "Twilight Oasis",
-    Q: "Uncategorized",
-    R: "Underground Facility",
-    S: "Urban Battleground",
-    T: "Volcanic",
-    U: "Sunqua Peak",
-};
+exports.fractalRotation = [
+  [6, 9, 2],
+  [19, 11, 8],
+  [0, 16, 14],
+  [3, 4, 15],
+  [2, 20, 5],
+  [8, 11, 12],
+  [3, 16, 18],
+  [5, 10, 7],
+  [15, 17, 6],
+  [13, 9, 19],
+  [11, 14, 1],
+  [17, 20, 18],
+  [3, 0, 8],
+  [10, 4, 6],
+  [13, 5, 12],
+];
